1410625a7d32529d05ac555c9ca1df033e434bde
Alexander Samoylov <Alexander.Samoylov@amd.com>
2026-03-06 16:17:33 +0100

    switching to oil https://github.com/stevearc/oil.nvim?tab=readme-ov-file

--- neovim.from.scratch/nvim/init.lua
+++ neovim.from.scratch/nvim/init.lua
@@ -25,16 +25,29 @@ require('lazy').setup ({
   build = ":TSUpdate"
 },
 
+-- {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--       "MunifTanjim/nui.nvim",
+--       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+--     }
+-- },
+
 {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
 },
+
 
 {
   "ibhagwan/fzf-lua",
